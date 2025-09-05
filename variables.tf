9b3539d3267a2ee53a50cb6960dc24744131436f
jlp77 <162034455+jlp77@users.noreply.github.com>
2025-09-05 12:03:16 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,14 +12,14 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "zone_public" {
-  description = "Zone publique"
+variable "zone_a" {
+  description = "Première zone de disponibilité"
   type        = string
   default     = "us-east-1a"
 }
 
-variable "zone_private" {
-  description = "Zone privée"
+variable "zone_b" {
+  description = "Deuxième zone de disponibilité"
   type        = string
   default     = "us-east-1b"
 }
