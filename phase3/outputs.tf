4459cbdca1a42f432175e2e85efcf3fa98c43c83
Portorobrine <64094407+Portorobrine@users.noreply.github.com>
2025-09-05 17:01:51 +0200

    Correction des références aux instances d'application dans les sorties pour utiliser le type 'webapp'.

--- phase3/outputs.tf
+++ phase3/outputs.tf
@@ -1,14 +1,14 @@
 output "app_instance_public_ips" {
     description = "Public IP addresses of the app instances"
-    value       = aws_instance.app[*].public_ip
+    value       = aws_instance.webapp[*].public_ip
 }
 
 output "app_instance_private_ips" {
     description = "Private IP addresses of the app instances"
-    value       = aws_instance.app[*].private_ip
+    value       = aws_instance.webapp[*].private_ip
 }
 
 output "app_instance_ids" {
     description = "IDs of the app instances"
-    value       = aws_instance.app[*].id
+    value       = aws_instance.webapp[*].id
 }
