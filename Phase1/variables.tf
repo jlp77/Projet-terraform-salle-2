14a43e9407488903e21e0a0d1db81a359eed87dd
jlp77 <162034455+jlp77@users.noreply.github.com>
2025-09-05 10:42:59 +0200

    update

--- Phase1/variables.tf
+++ Phase1/variables.tf
@@ -24,37 +24,37 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "web_public_zone1" {
+variable "web_public_subnet_cidr_1" {
   description = "Bloc réseau public pour le VPC"
   type        = string
   default     = "10.0.1.0/24"
 }
 
-variable "web_public_zone2" {
+variable "web_public_subnet_cidr_2" {
   description = "Bloc réseau public pour le VPC"
   type        = string
   default     = "10.0.2.0/24"
 }
 
-variable "app-private-zone1" {
+variable "app_private_subnet_cidr_1" {
   description = "Bloc réseau public pour le VPC"
   type        = string
   default     = "10.0.10.0/24"
 }
 
-variable "app-private-zone2" {
+variable "app_private_subnet_cidr_2" {
   description = "Bloc réseau public pour le VPC"
   type        = string
   default     = "10.0.20.0/24"
 }
 
-variable "db-private-zone1" {
+variable "db_private_subnet_cidr_1" {
   description = "Bloc réseau public pour le VPC"
   type        = string
   default     = "10.0.100.0/24"
 }
 
-variable "db-private-zone2" {
+variable "db_private_subnet_cidr_2" {
   description = "Bloc réseau public pour le VPC"
   type        = string
   default     = "10.0.200.0/24"
