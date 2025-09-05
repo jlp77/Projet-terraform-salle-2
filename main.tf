2312ae1123352c369ec75be258b854d4a067c19a
jlp77 <162034455+jlp77@users.noreply.github.com>
2025-09-05 09:55:02 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Use a recent version
+    }
+  }
+}
+
+# Provider AWS 
+provider "aws" {
+    region = var.location
+}
+
+# Création du VPC
+resource "aws_vpc" "ecosop-vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "ecosop-vpc"
+  }
+}
+
+# Sous réseau WEB TIER PUBLIC - ZONE 1
+resource "aws_subnet" "webtier_public_zone1" {
+  vpc_id                  = aws_vpc.ecosop-vpc.id
+  cidr_block              = var.webtier_public_subnet_cidr_1
+  map_public_ip_on_launch = true
+  availability_zone       = var.az_public
+  tags = {
+    Name = "webtier_public_subnet_cidr_1"
+  }
+}
+
+# Sous réseau WEB TIER PUBLIC - ZONE 2
+resource "aws_subnet" "webtier_public_zone2" {
+  vpc_id                  = aws_vpc.ecosop-vpc.id
+  cidr_block              = var.webtier_public_subnet_cidr_2
+  map_public_ip_on_launch = true
+  availability_zone       = var.az_public
+  tags = {
+    Name = "webtier_public_subnet_cidr_2"
+  }
+}
+
+# Sous réseau APP TIER PRIVE - ZONE 1
+resource "aws_subnet" "apptier-private-zone1" {
+  vpc_id                  = aws_vpc.ecosop-vpc.id
+  cidr_block              = var.apptier_private_subnet_cidr_1
+  map_public_ip_on_launch = true
+  availability_zone       = var.az_public
+  tags = {
+    Name = "apptier_public_subnet_cidr_1"
+  }
+}
+
+# Sous réseau APP TIER PRIVE - ZONE 2
+resource "aws_subnet" "apptier-private-zone2" {
+  vpc_id                  = aws_vpc.ecosop-vpc.id
+  cidr_block              = var.app_private_subnet_cidr_2
+  map_public_ip_on_launch = true
+  availability_zone       = var.az_public
+  tags = {
+    Name = "app_public_subnet_cidr_2"
+  }
+}
+
+# Sous réseau DB TIER PRIVE - ZONE 1
+resource "aws_subnet" "apptier-private-zone1" {
+  vpc_id                  = aws_vpc.ecosop-vpc.id
+  cidr_block              = var.db_private_subnet_cidr_1
+  map_public_ip_on_launch = true
+  availability_zone       = var.az_public
+  tags = {
+    Name = "db_private_subnet_cidr_1"
+  }
+}
+
+# Sous réseau DB TIER PRIVE - ZONE 2
+resource "aws_subnet" "apptier-private-zone2" {
+  vpc_id                  = aws_vpc.ecosop-vpc.id
+  cidr_block              = var.db_private_subnet_cidr_2
+  map_public_ip_on_launch = true
+  availability_zone       = var.az_public
+  tags = {
+    Name = "db_private_subnet_cidr_2"
+  }
+}
